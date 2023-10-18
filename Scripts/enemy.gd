f765e2d37fac6398a4d83d8c7bb6ac6319e4eda3
Eshwar <reshwar2004@gmail.com>
2023-10-19 00:08:20 +0530

    Impleted enemy hitboxes and hurtboxes

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,5 +2,15 @@ extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
 
+var health : int = 10
+
 func _ready():
 	animated_sprite.play("idle")
+	
+	var emitter = $HurtBox
+	emitter.take_damage.connect(take_damage)
+
+func take_damage():
+	health -= 1
+	if health <= 0:
+		queue_free()
--- Scripts/hurtbox.gd
+++ Scripts/hurtbox.gd
@@ -1,6 +1,7 @@
 class_name HurtBox
 extends Area2D
 
+signal take_damage
 
 func _ready():
 	connect("area_entered", _on_area_entered)
@@ -9,5 +10,4 @@ func _ready():
 func _on_area_entered(hitbox : HitBox) -> void:
 	if hitbox == null:
 		return
-	
 	emit_signal("take_damage")
